287080d9e49fe3f38e245191c8d708eaf0994222
newu2012 <newu2011@gmail.com>
2022-02-10 19:41:00 +0500

    init: init commit

---
+++ app/src/main/java/com/newu2012/doubletappcourse/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.newu2012.doubletappcourse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
